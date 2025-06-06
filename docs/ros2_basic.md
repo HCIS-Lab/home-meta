80b244a581dd27ba75a84b4ebf2b11edfe698d4d
En-Fu Liao <efliao@cs.nctu.edu.tw>
2025-06-07 01:15:38 +0800

    feat: setup script (for container) & update home.repos

---
+++ docs/ros2_basic.md
@@ -0,0 +1,14 @@
+# ROS2 Basics
+
+
+## Create a Package
+
+```
+# python
+ros2 pkg create --build-type ament_python <package_name>
+
+# cmake
+ros2 pkg create --build-type ament_cmake <package_name>
+```
+
+reference: https://docs.ros.org/en/humble/Tutorials/Beginner-Client-Libraries/Creating-Your-First-ROS2-Package.html
---
+++ scripts/setup.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+mkdir -p src
+vcs import src < home.repos
+
+# install python dependencies for yolo_ros
+pip install --upgrade -r src/yolo_ros/requirements.txt
+
+# install python dependencies for home_nlp
+pip install --upgrade -r src/home_nlp/requirements.txt
+
+# ros2 package dependencies
+rosdep update
+sudo apt-get update -y
+rosdep install --from-paths src --ignore-src --skip-keys=portaudio -r -y
+sudo apt-get install libportaudio2
